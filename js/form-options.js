c7b5cda7cbc3876b98019243c7e0dd2fd5d680a3
Irfan Fauzi <ervhan@gmail.com>
2015-10-09 15:29:09 +0700

    Coming soon text

--- js/form-options.js
+++
@@ -1,3 +0,0 @@
-    //<![CDATA[
-    window.leadpagesForm(null, [{"required": "Bagian ini Wajib diisi ya ^_^", "type": "text", "name": "name", "invalid": "Tolong diisi dengan data yang benar"}, {"required": "Bagian ini Wajib diisi ya ^_^", "type": "email", "name": "email", "invalid": "Tolong diisi dengan data yang benar"}], false);
-    //]]>
